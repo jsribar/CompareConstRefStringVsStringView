060a8ea95a7dce8c8bcb1a4896209bbbae572774
Julijan Šribar <julijan.sribar@gmail.com>
2023-12-18 18:26:39 +0100

    Add return value benchmark

--- CompareConstRefStringVsStringView/CompareConstRefStringVsStringView.cpp
+++ CompareConstRefStringVsStringView/CompareConstRefStringVsStringView.cpp
@@ -1,7 +1,7 @@
 // CompareConstRefStringVsStringView.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-// https://godbolt.org/z/zG95bxvM1
+// https://godbolt.org/z/nod7ThMPv
 
 #include <array>
 #include <chrono>
@@ -28,6 +28,11 @@ private:
     std::chrono::high_resolution_clock::time_point m_startPoint;
 };
 
+static constexpr int repeat{ 10'000 };
+const std::array<std::string, 6> text{ "Hello", "World", "Guten", "Morgen", "Dobro", "jutro" };
+//const std::array<const char*, 6> text{ "Hello", "World", "Guten", "Morgen", "Dobro", "jutro" };
+//const std::array<std::string_view, 6> text{ "Hello", "World", "Guten", "Morgen", "Dobro", "jutro" };
+
 class Process
 {
 public:
@@ -35,6 +40,12 @@ public:
     void ProcessString(Str txt)
     {
         ProcessChar(txt[0]);
+    }
+
+    template <typename Str>
+    Str ReturnString(int i)
+    {
+        return text[i % text.size()];
     }
 
     char ch{ 127 };
@@ -48,13 +59,8 @@ private:
     }
 };
 
-static constexpr int repeat{ 10'000 };
-const std::array<std::string, 6> text{ "Hello", "World", "Guten", "Morgen", "Dobro", "jutro" };
-//const std::array<const char*, 6> text{ "Hello", "World", "Guten", "Morgen", "Dobro", "jutro" };
-//const std::array<std::string_view, 6> text{ "Hello", "World", "Guten", "Morgen", "Dobro", "jutro" };
-
 template <typename Str>
-std::pair<double, int> PerformBenchmark(int count)
+std::pair<double, int> ArgumentBechmark(int count)
 {
     // Result of dummy processing. Used also to check if all benchmarks provide the same result.
     int result = 127;
@@ -83,15 +89,44 @@ std::pair<double, int> PerformBenchmark(int count)
     return { minDuration.count(), result };
 }
 
-
-int main()
+template <typename Str>
+std::pair<double, int> ReturnValueBechmark(int count)
 {
-    auto BenchmarkConstStringRef = PerformBenchmark<const std::string&>;
-    auto BenchmarkStringView = PerformBenchmark<std::string_view>;
-    auto BenchmarkConstStringViewRef = PerformBenchmark<const std::string_view&>;
-    auto BenchmarkBoostStringView = PerformBenchmark<boost::string_view>;
+    // Result of dummy processing. Used also to check if all benchmarks provide the same result.
+    int result = 127;
 
-    const int count = 300;
+    Process process;
+
+    std::chrono::duration<double> minDuration = std::chrono::duration<double>::max();
+
+    for (int n = 0; n < count; ++n)
+    {
+        StopWatch sw;
+
+        for (int i = 0; i < repeat; i++)
+        {
+            // Access strings interchangably to minimize or avoid cacheing or optimizations.
+            Str str = process.ReturnString<Str>(i);
+            result %= str[0];
+            result += 15;
+        }
+        if (const auto duration = sw.Stop(); duration < minDuration)
+        {
+            minDuration = duration;
+        }
+    }
+    // Return both duration and the result of dummy operation to avoid compiler to optimize out corresponding operations.
+    return { minDuration.count(), result };
+}
+
+void DoArgumentsBenchmark(int count)
+{
+    std::cout << "\n*** Different argument types:\n\n";
+
+    auto BenchmarkConstStringRef = ArgumentBechmark<const std::string&>;
+    auto BenchmarkStringView = ArgumentBechmark<std::string_view>;
+    auto BenchmarkConstStringViewRef = ArgumentBechmark<const std::string_view&>;
+    auto BenchmarkBoostStringView = ArgumentBechmark<boost::string_view>;
 
     double constStringRefDuration = 0;
     double stringViewDuration = 0;
@@ -165,4 +200,79 @@ int main()
     std::cout << resultConstStringViewRef << '\n';
     std::cout << "boost::string_view: " << boostStringViewDuration << "\n";
     std::cout << resultBoostStringView << '\n';
+}
+
+void DoReturnValueBenchmark(int count)
+{
+    std::cout << "\n*** Different return value types:\n\n";
+
+    auto BenchmarkConstStringRef = ReturnValueBechmark<const std::string&>;
+    auto BenchmarkStringView = ReturnValueBechmark<std::string_view>;
+    auto BenchmarkBoostStringView = ReturnValueBechmark<boost::string_view>;
+
+    double constStringRefDuration = 0;
+    double stringViewDuration = 0;
+    double boostStringViewDuration = 0;
+
+    int resultConstStringRef = 127;
+    int resultStringView = 127;
+    int resultBoostStringView = 127;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        // Do benchmark for each parameter type. Result of dummy processing is included to avoid optimizing it out.
+        {
+            const auto [duration, ch] = BenchmarkConstStringRef(count);
+            constStringRefDuration += duration;
+            resultConstStringRef %= ch;
+            resultConstStringRef += 15;
+        }
+        {
+            const auto [duration, ch] = BenchmarkStringView(count);
+            stringViewDuration += duration;
+            resultStringView %= ch;
+            resultStringView += 15;
+        }
+        {
+            auto [duration, ch] = BenchmarkBoostStringView(count);
+            boostStringViewDuration += duration;
+            resultBoostStringView %= ch;
+            resultBoostStringView += 15;
+        }
+        // Repeat benchmark for each parameter type in reverse order
+        {
+            auto [duration, ch] = BenchmarkBoostStringView(count);
+            boostStringViewDuration += duration;
+            resultBoostStringView %= ch;
+            resultBoostStringView += 15;
+        }
+        {
+            const auto [duration, ch] = BenchmarkStringView(count);
+            stringViewDuration += duration;
+            resultStringView %= ch;
+            resultStringView += 15;
+        }
+        {
+            const auto [duration, ch] = BenchmarkConstStringRef(count);
+            constStringRefDuration += duration;
+            resultConstStringRef %= ch;
+            resultConstStringRef += 15;
+        }
+    }
+
+    std::cout << "const std::string&: " << constStringRefDuration << "\n";
+    std::cout << resultConstStringRef << '\n';
+    std::cout << "std::string_view: " << stringViewDuration << "\n";
+    std::cout << resultStringView << '\n';
+    std::cout << "boost::string_view: " << boostStringViewDuration << "\n";
+    std::cout << resultBoostStringView << '\n';
+}
+
+int main()
+{
+    const int count = 300;
+
+    DoArgumentsBenchmark(count);
+
+    DoReturnValueBenchmark(count);
 }
