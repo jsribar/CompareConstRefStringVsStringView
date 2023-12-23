1dd39f6e438cb00fde118602c02e1f61898fe5df
Julijan Šribar <julijan.sribar@gmail.com>
2023-12-23 17:26:35 +0100

    Extend tests for return value with const boost::string_view&

--- CompareConstRefStringVsStringView/CompareConstRefStringVsStringView.cpp
+++ CompareConstRefStringVsStringView/CompareConstRefStringVsStringView.cpp
@@ -18,7 +18,7 @@ public:
         m_startPoint = std::chrono::high_resolution_clock::now();
     }
 
-    std::chrono::duration<double> Stop()
+    std::chrono::duration<double> Stop() const
     {
         auto endPoint = std::chrono::high_resolution_clock::now();
         return std::chrono::duration_cast<std::chrono::duration<double>>(endPoint - m_startPoint);
@@ -28,14 +28,20 @@ private:
     std::chrono::high_resolution_clock::time_point m_startPoint;
 };
 
-static constexpr int repeat{ 10'000 };
-const std::array<std::string, 6> text{ "Hello", "World", "Guten", "Morgen", "Dobro", "jutro" };
+static constexpr int repeat{ 5'000 };
+static constexpr int loops{ 30 };
+const std::array<std::string, 6> string_array{ "Hello", "World", "Guten", "Morgen", "Dobro", "jutro" };
 //const std::array<const char*, 6> text{ "Hello", "World", "Guten", "Morgen", "Dobro", "jutro" };
-//const std::array<std::string_view, 6> text{ "Hello", "World", "Guten", "Morgen", "Dobro", "jutro" };
+const std::array<boost::string_view, 6> string_view_array{ "Hello", "World", "Guten", "Morgen", "Dobro", "jutro" };
 
+template <typename Array>
 class Process
 {
 public:
+    explicit Process(const Array& array) : array(array)
+    {
+    }
+
     template <typename Str>
     void ProcessString(Str txt)
     {
@@ -45,7 +51,7 @@ public:
     template <typename Str>
     Str ReturnString(int i)
     {
-        return text[i % text.size()];
+        return array[i % array.size()];
     }
 
     char ch{ 127 };
@@ -57,15 +63,17 @@ private:
         ch %= c;
         ch += 15;
     }
+
+    const Array& array;
 };
 
-template <typename Str>
-std::pair<double, int> ArgumentBechmark(int count)
+template <typename Str, typename Array>
+std::pair<double, int> ArgumentBechmark(int count, const Array& array)
 {
     // Result of dummy processing. Used also to check if all benchmarks provide the same result.
     int result = 127;
 
-    Process process;
+    Process process(array);
 
     std::chrono::duration<double> minDuration = std::chrono::duration<double>::max();
 
@@ -75,8 +83,8 @@ std::pair<double, int> ArgumentBechmark(int count)
 
         for (int i = 0; i < repeat; i++)
         {
-            // Access strings interchangably to minimize or avoid cacheing or optimizations.
-            process.ProcessString<Str>(text[i % text.size()]);
+            // Access strings interchangably to minimize or avoid caching or optimizations.
+            process.ProcessString(array[i % array.size()]);
         }
         if (const auto duration = sw.Stop(); duration < minDuration)
         {
@@ -89,13 +97,13 @@ std::pair<double, int> ArgumentBechmark(int count)
     return { minDuration.count(), result };
 }
 
-template <typename Str>
-std::pair<double, int> ReturnValueBechmark(int count)
+template <typename Str, typename Array>
+std::pair<double, int> ReturnValueBechmarkStringArray(int count, const Array& array)
 {
     // Result of dummy processing. Used also to check if all benchmarks provide the same result.
     int result = 127;
 
-    Process process;
+    Process process(array);
 
     std::chrono::duration<double> minDuration = std::chrono::duration<double>::max();
 
@@ -106,7 +114,7 @@ std::pair<double, int> ReturnValueBechmark(int count)
         for (int i = 0; i < repeat; i++)
         {
             // Access strings interchangably to minimize or avoid cacheing or optimizations.
-            Str str = process.ReturnString<Str>(i);
+            Str str = process.template ReturnString<Str>(i);
             result %= str[0];
             result += 15;
         }
@@ -123,10 +131,10 @@ void DoArgumentsBenchmark(int count)
 {
     std::cout << "\n*** Different argument types:\n\n";
 
-    auto BenchmarkConstStringRef = ArgumentBechmark<const std::string&>;
-    auto BenchmarkStringView = ArgumentBechmark<std::string_view>;
-    auto BenchmarkConstStringViewRef = ArgumentBechmark<const std::string_view&>;
-    auto BenchmarkBoostStringView = ArgumentBechmark<boost::string_view>;
+    auto BenchmarkConstStringRefStringArray = ArgumentBechmark<const std::string&, decltype(string_array)>;
+    auto BenchmarkStringViewStringArray = ArgumentBechmark<std::string_view, decltype(string_array)>;
+    auto BenchmarkConstStringViewRefStringArray = ArgumentBechmark<const std::string_view&, decltype(string_array)>;
+    auto BenchmarkBoostStringViewStringArray = ArgumentBechmark<boost::string_view, decltype(string_array)>;
 
     double constStringRefDuration = 0;
     double stringViewDuration = 0;
@@ -138,54 +146,54 @@ void DoArgumentsBenchmark(int count)
     int resultConstStringViewRef = 127;
     int resultBoostStringView = 127;
 
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < loops; ++i)
     {
         // Do benchmark for each parameter type. Result of dummy processing is included to avoid optimizing it out.
         {
-            const auto [duration, ch] = BenchmarkConstStringRef(count);
+            const auto [duration, ch] = BenchmarkConstStringRefStringArray(count, string_array);
             constStringRefDuration += duration;
             resultConstStringRef %= ch;
             resultConstStringRef += 15;
         }
         {
-            const auto [duration, ch] = BenchmarkStringView(count);
+            const auto [duration, ch] = BenchmarkStringViewStringArray(count, string_array);
             stringViewDuration += duration;
             resultStringView %= ch;
             resultStringView += 15;
         }
         {
-            const auto [duration, ch] = BenchmarkConstStringViewRef(count);
+            const auto [duration, ch] = BenchmarkConstStringViewRefStringArray(count, string_array);
             constStringViewRefDuration += duration;
             resultConstStringViewRef %= ch;
             resultConstStringViewRef += 15;
         }
         {
-            auto [duration, ch] = BenchmarkBoostStringView(count);
+            auto [duration, ch] = BenchmarkBoostStringViewStringArray(count, string_array);
             boostStringViewDuration += duration;
             resultBoostStringView %= ch;
             resultBoostStringView += 15;
         }
         // Repeat benchmark for each parameter type in reverse order
         {
-            auto [duration, ch] = BenchmarkBoostStringView(count);
+            auto [duration, ch] = BenchmarkBoostStringViewStringArray(count, string_array);
             boostStringViewDuration += duration;
             resultBoostStringView %= ch;
             resultBoostStringView += 15;
         }
         {
-            const auto [duration, ch] = BenchmarkConstStringViewRef(count);
+            const auto [duration, ch] = BenchmarkConstStringViewRefStringArray(count, string_array);
             constStringViewRefDuration += duration;
             resultConstStringViewRef %= ch;
             resultConstStringViewRef += 15;
         }
         {
-            const auto [duration, ch] = BenchmarkStringView(count);
+            const auto [duration, ch] = BenchmarkStringViewStringArray(count, string_array);
             stringViewDuration += duration;
             resultStringView %= ch;
             resultStringView += 15;
         }
         {
-            const auto [duration, ch] = BenchmarkConstStringRef(count);
+            const auto [duration, ch] = BenchmarkConstStringRefStringArray(count, string_array);
             constStringRefDuration += duration;
             resultConstStringRef %= ch;
             resultConstStringRef += 15;
@@ -206,54 +214,86 @@ void DoReturnValueBenchmark(int count)
 {
     std::cout << "\n*** Different return value types:\n\n";
 
-    auto BenchmarkConstStringRef = ReturnValueBechmark<const std::string&>;
-    auto BenchmarkStringView = ReturnValueBechmark<std::string_view>;
-    auto BenchmarkBoostStringView = ReturnValueBechmark<boost::string_view>;
+    auto BenchmarkConstStringRefStringArray = ReturnValueBechmarkStringArray<const std::string&, decltype(string_array)>;
+    auto BenchmarkStringViewStringArray = ReturnValueBechmarkStringArray<std::string_view, decltype(string_array)>;
+    auto BenchmarkBoostStringViewStringArray = ReturnValueBechmarkStringArray<boost::string_view, decltype(string_array)>;
+    auto BenchmarkBoostStringViewStringViewArray = ReturnValueBechmarkStringArray<boost::string_view, decltype(string_view_array)>;
+    auto BenchmarkConstBoostStringViewRefStringViewArray = ReturnValueBechmarkStringArray<const boost::string_view&, decltype(string_view_array)>;
 
     double constStringRefDuration = 0;
     double stringViewDuration = 0;
     double boostStringViewDuration = 0;
 
+    double boostStringViewDuration2 = 0;
+    double constBoostStringViewRefDuration = 0;
+
     int resultConstStringRef = 127;
     int resultStringView = 127;
     int resultBoostStringView = 127;
 
-    for (int i = 0; i < 10; ++i)
+    int resultBoostStringView2 = 127;
+    int resultConstBoostStringViewRef = 127;
+
+    for (int i = 0; i < loops; ++i)
     {
         // Do benchmark for each parameter type. Result of dummy processing is included to avoid optimizing it out.
         {
-            const auto [duration, ch] = BenchmarkConstStringRef(count);
+            const auto [duration, ch] = BenchmarkConstStringRefStringArray(count, string_array);
             constStringRefDuration += duration;
             resultConstStringRef %= ch;
             resultConstStringRef += 15;
         }
         {
-            const auto [duration, ch] = BenchmarkStringView(count);
+            const auto [duration, ch] = BenchmarkStringViewStringArray(count, string_array);
             stringViewDuration += duration;
             resultStringView %= ch;
             resultStringView += 15;
         }
         {
-            auto [duration, ch] = BenchmarkBoostStringView(count);
+            auto [duration, ch] = BenchmarkBoostStringViewStringArray(count, string_array);
             boostStringViewDuration += duration;
             resultBoostStringView %= ch;
             resultBoostStringView += 15;
+        }
+        {
+            auto [duration, ch] = BenchmarkBoostStringViewStringViewArray(count, string_view_array);
+            boostStringViewDuration2 += duration;
+            resultBoostStringView2 %= ch;
+            resultBoostStringView2 += 15;
+        }
+        {
+            auto [duration, ch] = BenchmarkConstBoostStringViewRefStringViewArray(count, string_view_array);
+            constBoostStringViewRefDuration += duration;
+            resultConstBoostStringViewRef %= ch;
+            resultConstBoostStringViewRef += 15;
         }
         // Repeat benchmark for each parameter type in reverse order
         {
-            auto [duration, ch] = BenchmarkBoostStringView(count);
+            auto [duration, ch] = BenchmarkConstBoostStringViewRefStringViewArray(count, string_view_array);
+            constBoostStringViewRefDuration += duration;
+            resultConstBoostStringViewRef %= ch;
+            resultConstBoostStringViewRef += 15;
+        }
+        {
+            auto [duration, ch] = BenchmarkBoostStringViewStringViewArray(count, string_view_array);
+            boostStringViewDuration2 += duration;
+            resultBoostStringView2 %= ch;
+            resultBoostStringView2 += 15;
+        }
+        {
+            auto [duration, ch] = BenchmarkBoostStringViewStringArray(count, string_array);
             boostStringViewDuration += duration;
             resultBoostStringView %= ch;
             resultBoostStringView += 15;
         }
         {
-            const auto [duration, ch] = BenchmarkStringView(count);
+            const auto [duration, ch] = BenchmarkStringViewStringArray(count, string_array);
             stringViewDuration += duration;
             resultStringView %= ch;
             resultStringView += 15;
         }
         {
-            const auto [duration, ch] = BenchmarkConstStringRef(count);
+            const auto [duration, ch] = BenchmarkConstStringRefStringArray(count, string_array);
             constStringRefDuration += duration;
             resultConstStringRef %= ch;
             resultConstStringRef += 15;
@@ -266,6 +306,10 @@ void DoReturnValueBenchmark(int count)
     std::cout << resultStringView << '\n';
     std::cout << "boost::string_view: " << boostStringViewDuration << "\n";
     std::cout << resultBoostStringView << '\n';
+    std::cout << "boost::string_view2: " << boostStringViewDuration2 << "\n";
+    std::cout << resultBoostStringView2 << '\n';
+    std::cout << "const boost::string_view&: " << constBoostStringViewRefDuration << "\n";
+    std::cout << resultConstBoostStringViewRef << '\n';
 }
 
 int main()
